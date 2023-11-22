a238c4cd16fc1b9ac5df3b83cb463983502d82c7
Moritz M <moritz.mlejnek@gmail.com>
2023-11-22 15:16:21 +0100

    IntIntSubtraktionTest

---
+++ app/src/test/java/hsw/gradle/template/SubtraktionTest.java
@@ -0,0 +1,29 @@
+package hsw.gradle.template;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class SubtraktionTest {
+
+    @BeforeAll
+    public static void beforAll() {
+        System.out.println("Start der Test für Subtraktion");
+    }
+
+    @Test
+    public void subtraktionIntInt() {
+        Subtraktion subtract = new Subtraktion();
+        int a = 11;
+        int b = 4;
+        int differenz = a - b;
+        Assertions.assertEquals(subtract.subtract(a, b), differenz, "Subtraktion der beiden Int erfolgreich");
+    }
+
+
+    @AfterAll
+    public void afterAll(){
+        System.out.println("Ende des Tests für Subtraktion");
+    }
+}
