aef105b5c1c834ff11e8978a51b2cfc012de3b5c
JoshuaVaartmann <joshua.vaartmann@gmail.com>
2023-11-22 12:02:51 +0100

    Hinzufügen von AdditionTest und der Testmethode für int und int

---
+++ app/src/test/java/hsw/gradle/template/AdditionTest.java
@@ -0,0 +1,22 @@
+package hsw.gradle.template;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class AdditionTest {
+
+    @BeforeAll
+    public void beforAll() {
+        System.out.println("Start der Test für addition");
+    }
+
+    @Test
+    public void additionIntInt() {
+        Addition addition = new Addition();
+        int a = 2;
+        int b = 3;
+        int summe = 2 + 3;
+        Assertions.assertEquals(addition.add(a, b), summe);
+    }
+}
