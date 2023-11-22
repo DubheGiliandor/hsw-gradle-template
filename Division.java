952ff447dcb0e4b834f0094effb4f2c91d338d8d
96Leon02 <leon.aschemann@hsw-stud.de>
2023-11-22 12:40:49 +0100

    Divion zweier Zahlen, int/int, double/double, int/double, double/int

--- Division.java
+++ Division.java
@@ -1,43 +1,48 @@
 public class Division {
 
-    int int_a;
-    int int_b;
-    double double_a;
-    double double_b;
 
-    public void zweiIntZahlen(){
-        if(int_b == 0){
-            int ergebnis = int_a/int_b;
+
+    public int zweiIntZahlen(int a, int b){
+        int ergebnis= 0;
+        if(b == 0){
+             ergebnis = a/b;
             System.out.println("Das Ergebnis lautet: "+ ergebnis);
         }else{
          System.err.println("Division durch 0 nicht möglich");
         }
+        return ergebnis;
     }
 
-    public void zweiDoubleZahlen(){
-        if(double_b == 0){
-            double ergebnis = double_a/double_b;
+    public double zweiDoubleZahlen(double a, double b){
+        double ergebnis= 0;
+        if(b == 0){
+             ergebnis = a/b;
             System.out.println("Das Ergebnis lautet: "+ ergebnis);
         }else{
             System.err.println("Division durch 0 nicht möglich");
         }
+        return ergebnis;
     }
 
-    public void intDurchDouble(){
-        if(double_b == 0){
-            double ergebnis = int_a/double_b;
+    public double intDurchDouble( int a, double b){
+        double ergebnis = 0;
+        if(b == 0){
+             ergebnis = a/b;
             System.out.println("Das Ergebnis lautet: "+ ergebnis);
         }else{
             System.err.println("Division durch 0 nicht möglich");
         }
+        return ergebnis;
     }
 
-    public void doubleDurchInt(){
-        if(int_b == 0){
-            double ergebnis = double_a/int_b;
+    public double doubleDurchInt(double a, int b){
+        double ergebnis = 0;
+        if(b == 0){
+             ergebnis = a/b;
             System.out.println("Das Ergebnis lautet: "+ ergebnis);
         }else{
             System.err.println("Division durch 0 nicht möglich");
         }
+        return ergebnis;
     }
 }
