1d9d3f0d58fb13d587c7eef33aab316ddabdb5b6
DubheGiliandor <julian.b.w.m.hansmann@googlemail.com>
2023-11-24 15:16:19 +0100

    Commit 12: MultiplikationTest aktiviert

--- app/src/test/java/hsw/gradle/template/MultiplikationTest.java
+++ app/src/test/java/hsw/gradle/template/MultiplikationTest.java
@@ -11,45 +11,45 @@ public class MultiplikationTest {
         System.out.println("Start der Test für multiplikation");
     }
 
-//    @Test
-//    public void multiplikationIntInt() {
-//        Multiplikation multiplikation = new Multiplikation();
-//        int a = 2;
-//        int b = 3;
-//        int produkt = a * b;
-//        Assertions.assertEquals(multiplikation.multiply(a, b), produkt,
-//                "Int und Int ist erfrolgreich");
-//    }
-//
-//    @Test
-//    public void multiplikationIntDouble() {
-//        Multiplikation multiplikation = new Multiplikation();
-//        int a = 2;
-//        double b = 3.22;
-//        double produkt = a * b;
-//        Assertions.assertEquals(multiplikation.multiply(a, b), produkt,
-//                "Int und Double ist erfrolgreich");
-//    }
-//
-//    @Test
-//    public void multiplikationDoubleInt() {
-//        Multiplikation multiplikation = new Multiplikation();
-//        double a = 2.32;
-//        int b = 3;
-//        double produkt = a * b;
-//        Assertions.assertEquals(multiplikation.multiply(a, b), produkt,
-//                "Double und Int ist erfrolgreich");
-//    }
-//
-//    @Test
-//    public void multiplikationDoubleDouble() {
-//        Multiplikation multiplikation = new Multiplikation();
-//        double a = 2.32;
-//        double b = 3.78;
-//        double produkt = a * b;
-//        Assertions.assertEquals(multiplikation.multiply(a, b), produkt,
-//                "Double und Double ist erfrolgreich");
-//    }
+    @Test
+    public void multiplikationIntInt() {
+        Multiplikation multiplikation = new Multiplikation();
+        int a = 2;
+        int b = 3;
+        int produkt = a * b;
+        Assertions.assertEquals(multiplikation.multiply(a, b), produkt,
+                "Int und Int ist erfrolgreich");
+    }
+
+    @Test
+    public void multiplikationIntDouble() {
+        Multiplikation multiplikation = new Multiplikation();
+        int a = 2;
+        double b = 3.22;
+        double produkt = a * b;
+        Assertions.assertEquals(multiplikation.multiply(a, b), produkt,
+                "Int und Double ist erfrolgreich");
+    }
+
+    @Test
+    public void multiplikationDoubleInt() {
+        Multiplikation multiplikation = new Multiplikation();
+        double a = 2.32;
+        int b = 3;
+        double produkt = a * b;
+        Assertions.assertEquals(multiplikation.multiply(a, b), produkt,
+                "Double und Int ist erfrolgreich");
+    }
+
+    @Test
+    public void multiplikationDoubleDouble() {
+        Multiplikation multiplikation = new Multiplikation();
+        double a = 2.32;
+        double b = 3.78;
+        double produkt = a * b;
+        Assertions.assertEquals(multiplikation.multiply(a, b), produkt,
+                "Double und Double ist erfrolgreich");
+    }
 
     @AfterAll
     public static void afterAll() {
